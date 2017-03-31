3ab13e055330cf3bae826baf9379e0ddfcd132f1
cristianCastrillon <cacastrillonfranco@gmail.com>
2017-03-31 15:12:37 -0500

    Revert "proyecto NetoCafe"
    This reverts commit 43cdaadd91671c695bbd1b855aab64cb926e3e66.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Ingresar a NetoCafé</b>
+            <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
         </div><!-- /.login-logo -->
 
     @if (count($errors) > 0)
@@ -23,34 +23,35 @@
     @endif
 
     <div class="login-box-body">
-    <p class="login-box-msg"> {{ trans('Inicie Sección') }} </p>
+    <p class="login-box-msg"> {{ trans('adminlte_lang::message.siginsession') }} </p>
     <form action="{{ url('/login') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group has-feedback">
-            <input type="email" class="form-control" placeholder="{{ trans('ejemplo@gmail.com') }}" name="email"/>
+            <input type="email" class="form-control" placeholder="{{ trans('adminlte_lang::message.email') }}" name="email"/>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="{{ trans('Contraseña') }}" name="password"/>
+            <input type="password" class="form-control" placeholder="{{ trans('adminlte_lang::message.password') }}" name="password"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
             <div class="col-xs-8">
                 <div class="checkbox icheck">
                     <label>
-                        <input type="checkbox" name="remember"> {{ trans('Recordar') }}
+                        <input type="checkbox" name="remember"> {{ trans('adminlte_lang::message.remember') }}
                     </label>
                 </div>
             </div><!-- /.col -->
             <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('Ingresar') }}</button>
+                <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
             </div><!-- /.col -->
         </div>
     </form>
 
+    @include('auth.partials.social_login')
 
-    <a href="{{ url('/password/reset') }}">{{ trans('Olvide mi contraseña') }}</a><br>
-    <a href="{{ url('/register') }}" class="text-center">{{ trans('Registrarme') }}</a>
+    <a href="{{ url('/password/reset') }}">{{ trans('adminlte_lang::message.forgotpassword') }}</a><br>
+    <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a>
 
 </div><!-- /.login-box-body -->
 
